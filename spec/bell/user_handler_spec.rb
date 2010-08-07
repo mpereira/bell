1bbb08f006ca9f20a45762f67694422cb88521fc
Murilo Soares Pereira <murilo.soares3@gmail.com>
2010-08-07 00:08:21 -0300

    Rearranging spec files.

--- spec/bell/user_handler_spec.rb
+++ spec/bell/user_handler_spec.rb
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__), '..', '..', 'spec_helper')
+require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe Bell::UserHandler do
   let(:args) { mock("args").as_null_object }
