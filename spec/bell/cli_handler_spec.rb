abff4b46d11d22db7917ae9dd5bf59700664c505
Murilo Soares Pereira <murilo.soares3@gmail.com>
2010-08-21 20:44:53 -0300

    Refactored CLI handler spec.

--- spec/bell/cli_handler_spec.rb
+++ spec/bell/cli_handler_spec.rb
@@ -1,28 +1,25 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe Bell::CliHandler do
-  let(:args) { mock("args") }
-  let(:user_args) { mock("user_args") }
-  let(:cli_handler) { Bell::CliHandler.new(args) }
-  let(:messenger) { mock(Bell::Messenger) }
+  let(:args) { mock("args").as_null_object }
+  let(:messenger) { mock("messenger") }
   let(:user_handler) { mock(Bell::UserHandler) }
+  let(:cli_handler) { Bell::CliHandler.new(messenger) }
 
   context "when calling for an invalid resource" do
     it "shows the usage" do
       args.stub!(:first).and_return('foo')
-      cli_handler.stub!(:messenger).and_return(messenger)
-      messenger.should_receive(:show_usage)
-      cli_handler.run
+      messenger.should_receive(:puts).with(Bell::Messenger.show_usage)
+      cli_handler.run(args)
     end
   end
 
   context "when calling for the user resource" do
     it "forwards the arguments to the user handler" do
       args.stub!(:first).and_return('user')
-      args.should_receive(:[]).with(1..-1).and_return(user_args)
-      Bell::UserHandler.should_receive(:new).with(user_args).and_return(user_handler)
+      Bell::UserHandler.should_receive(:new).with(messenger).and_return(user_handler)
       user_handler.should_receive(:run)
-      cli_handler.run
+      cli_handler.run(args)
     end
   end
 end
