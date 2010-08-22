305a467f7e049e3f91ae4097b781b63cdd9603ba
Murilo Soares Pereira <murilo.soares3@gmail.com>
2010-08-21 21:54:51 -0300

    Refactored user creator specs.

--- spec/bell/user_creator_spec.rb
+++ spec/bell/user_creator_spec.rb
@@ -2,25 +2,24 @@ require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe Bell::UserCreator do
   let(:args) { mock("args").as_null_object }
-  let(:user_creator) { described_class.new(args) }
-  let(:user) { mock(Bell::User).as_null_object }
-  let(:messenger) { mock(Bell::Messenger) }
+  let(:messenger) { mock(StringIO) }
+  let(:user) { mock(Bell::User) }
+  let(:user_name) { mock("user_name") }
+  let(:user_creator) { described_class.new(messenger) }
 
   context "when no user name is passed" do
     it "shows the usage" do
       args.stub!(:length).and_return(0)
-      user_creator.stub!(:messenger).and_return(messenger)
-      messenger.should_receive(:show_usage)
-      user_creator.run
+      messenger.should_receive(:puts).with(Bell::Messenger.show_usage)
+      user_creator.run(args)
     end
   end
 
   context "when the user name is passed together with other arguments" do
     it "shows the usage" do
       args.stub!(:length).and_return(2)
-      user_creator.stub!(:messenger).and_return(messenger)
-      messenger.should_receive(:show_usage)
-      user_creator.run
+      messenger.should_receive(:puts).with(Bell::Messenger.show_usage)
+      user_creator.run(args)
     end
   end
 
@@ -28,23 +27,20 @@ describe Bell::UserCreator do
     context "when the given user doesn't exist" do
       it "creates the user" do
         args.stub!(:length).and_return(1)
-        user_creator.stub!(:messenger).and_return(messenger)
-        Bell::User.should_receive(:new).with(args).and_return(user)
-        user.should_receive(:exists?).and_return(false)
-        user.should_receive(:create)
-        messenger.should_receive(:notify_user_creation).with(user.name)
-        user_creator.run
+        args.stub!(:first).and_return(user_name)
+        Bell::User.stub!(:[]).with(:name => user_name).and_return(nil)
+        messenger.should_receive(:puts).with(Bell::Messenger.notify_user_creation(user_name))
+        user_creator.run(args)
       end
     end
 
     context "when the given user exist" do
       it "shows a message saying that a user with that name exists" do
         args.stub!(:length).and_return(1)
-        user_creator.stub!(:messenger).and_return(messenger)
-        Bell::User.should_receive(:new).with(args).and_return(user)
-        user.should_receive(:exists?).and_return(true)
-        messenger.should_receive(:notify_user_existence).with(user.name)
-        user_creator.run
+        args.stub!(:first).and_return(user_name)
+        Bell::User.stub!(:[]).with(:name => user_name).and_return(user)
+        messenger.should_receive(:puts).with(Bell::Messenger.notify_user_existence(user_name))
+        user_creator.run(args)
       end
     end
   end
