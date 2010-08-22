c8421d2e9c068a5e7cda43be54655cd3d941c5af
Murilo Soares Pereira <murilo.soares3@gmail.com>
2010-08-22 01:16:16 -0300

    I don't need to run the bare script from cucumber anymore.

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,5 +2,3 @@ $:.unshift File.join(File.dirname(__FILE__), '..', '..', 'lib')
 require 'aruba'
 require 'bell'
 require 'spec/stubs/cucumber'
-
-ENV['PATH'] += ":#{File.join(File.dirname(__FILE__), '..', '..', 'bin')}"
