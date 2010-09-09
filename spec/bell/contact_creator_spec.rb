0af9c2774c759af8f6ea3e1759aafbdd4271712d
Murilo Soares Pereira <murilo.soares3@gmail.com>
2010-09-09 00:04:04 -0300

    Updated the specs for the contact handler and erased invalid specs.

--- spec/bell/contact_creator_spec.rb
+++ spec/bell/contact_creator_spec.rb
@@ -1,57 +1,6 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe Bell::ContactCreator do
-  let(:args) { mock("args").as_null_object }
   let(:messenger) { mock("messenger") }
-  let(:user_does_not_exist) { mock("user_does_not_exist") }
-  let(:contact_name_taken) { mock("contact_name_taken") }
-  let(:contact_created) { mock("contact_created") }
-  let(:user) { mock(Bell::User) }
-  let(:contact) { mock(Bell::Contact) }
   let(:contact_creator) { described_class.new(messenger) }
-
-  context "when the arguments passed are invalid" do
-    it "raises ContactCreatorArgumentError" do
-      contact_creator.stub!(:valid_contact_creation_args?).and_return(false)
-      lambda { contact_creator.create!(args) }.
-        should raise_error(Bell::Errors::ContactCreatorArgumentError)
-    end
-  end
-
-  context "when the arguments passed are valid" do
-    context "when the user passed as argument doesn't exist" do
-      it "tells us that the user doesn't exist" do
-        contact_creator.stub!(:valid_contact_creation_args?).and_return(true)
-        Bell::User.stub!(:find).and_return(nil)
-        Bell::OutputFormatter.stub!(:user_does_not_exist).and_return(user_does_not_exist)
-        messenger.should_receive(:puts).with(user_does_not_exist)
-        contact_creator.create!(args)
-      end
-    end
-
-    context "when the user passed as argument exists" do
-      context "when the contact passed as argument already exists" do
-        it "tells us that the already exists" do
-          contact_creator.stub!(:valid_contact_creation_args?).and_return(true)
-          Bell::User.stub!(:find).and_return(user)
-          Bell::Contact.stub!(:find).and_return(contact)
-          Bell::OutputFormatter.stub!(:contact_name_taken).and_return(contact_name_taken)
-          messenger.should_receive(:puts).with(contact_name_taken)
-          contact_creator.create!(args)
-        end
-      end
-
-      context "when the contact passed as argument doesn't exist" do
-        it "creates the contact and tells us that it was created" do
-          contact_creator.stub!(:valid_contact_creation_args?).and_return(true)
-          Bell::User.stub!(:find).and_return(user)
-          Bell::Contact.stub!(:find).and_return(nil)
-          user.should_receive(:add_contact)
-          Bell::OutputFormatter.stub!(:contact_created).and_return(contact_created)
-          messenger.should_receive(:puts).with(contact_created)
-          contact_creator.create!(args)
-        end
-      end
-    end
-  end
 end
--- spec/bell/contact_handler_spec.rb
+++ spec/bell/contact_handler_spec.rb
@@ -1,35 +1,74 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe Bell::ContactHandler do
-  let(:args) { mock("args").as_null_object }
-  let(:messenger) { mock("messenger").as_null_object }
+  let(:messenger) { mock("messenger") }
   let(:contact_creator) { mock(Bell::ContactCreator) }
   let(:contact_lister) { mock(Bell::ContactLister) }
   let(:contact_handler) { described_class.new(messenger) }
 
-  context "handling an invalid action" do
-    it "shows the usage" do
-      args.stub!(:first).and_return('foo')
-      lambda { contact_handler.handle!(args) }.should
+  context "when given an invalid action" do
+    let(:invalid_action) { %w[foo] }
+
+    it "raises ContactHandlerArgumentError" do
+      lambda { contact_handler.handle!(invalid_action) }.should
         raise_error(Bell::Errors::ContactHandlerArgumentError)
     end
   end
 
-  context "handling the 'create' action" do
-    it "creates a contact creator instance to handle the creation" do
-      args.stub!(:first).and_return('create')
-      Bell::ContactCreator.should_receive(:new).with(messenger).and_return(contact_creator)
-      contact_creator.should_receive(:create!)
-      contact_handler.handle!(args)
+  context "when given the 'create' action" do
+    context "with valid arguments" do
+      let(:valid_create_action) { %w[create augusto -n 1234123412 -u murilo] }
+
+      it "forwards the arguments to the contact creator" do
+        Bell::ContactCreator.should_receive(:new).
+          with(messenger).and_return(contact_creator)
+        contact_creator.should_receive(:create)
+        contact_handler.handle!(valid_create_action)
+      end
+    end
+
+    context "with invalid arguments" do
+      let(:invalid_create_action) { %w[create augusto n 1234123412 u murilo] }
+
+      it "shows the usage" do
+        messenger.should_receive(:puts).with(Bell::USAGE)
+        contact_handler.handle!(invalid_create_action)
+      end
     end
   end
 
-  context "handling the 'list' action" do
-    it "creates a contact lister instance to handle the listing" do
-      args.stub!(:first).and_return('list')
-      Bell::ContactLister.should_receive(:new).with(messenger).and_return(contact_lister)
-      contact_lister.should_receive(:list!)
-      contact_handler.handle!(args)
+  context "when given the 'list' action" do
+    context "with valid arguments" do
+      context "when not specifying a user" do
+        let(:valid_create_action) { %w[list] }
+
+        it "forwards the arguments to the contact lister" do
+          Bell::ContactLister.should_receive(:new).
+            with(messenger).and_return(contact_lister)
+          contact_lister.should_receive(:list).with({})
+          contact_handler.handle!(valid_create_action)
+        end
+      end
+
+      context "when specifying a user" do
+        let(:valid_create_action) { %w[list murilo] }
+
+        it "forwards the arguments to the contact lister" do
+          Bell::ContactLister.should_receive(:new).
+            with(messenger).and_return(contact_lister)
+          contact_lister.should_receive(:list).with(:name => 'murilo')
+          contact_handler.handle!(valid_create_action)
+        end
+      end
+    end
+
+    context "with invalid arguments" do
+      let(:invalid_list_action) { %w[list murilo foo] }
+
+      it "shows the usage" do
+        messenger.should_receive(:puts).with(Bell::USAGE)
+        contact_handler.handle!(invalid_list_action)
+      end
     end
   end
 end
