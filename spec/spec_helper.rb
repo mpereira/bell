686cd8ebac384b826e5a74920de6e480efe0e56c
Murilo Soares Pereira <murilo.soares3@gmail.com>
2010-10-17 08:03:29 -0200

    This is supposed to suppress the deprecation warnings.. but it isn't.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,10 @@ unless defined?(FIXTURES_PATH)
   FIXTURES_PATH = File.join(File.dirname(__FILE__), 'fixtures')
 end
 
+Spec::Runner.configure do |config|
+  config.suppress_deprecation_warnings!
+end
+
 class Spec::Example::ExampleGroup
   def execute(*args, &block)
     DB.transaction do
