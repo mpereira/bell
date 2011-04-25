cd66bd04c2d8139efc51bea7aa62c642a81095d5
Murilo Pereira <murilo@murilopereira.com>
2011-04-25 04:28:20 -0300

    Cross-model number uniqueness, please.

--- lib/bell/public_contact.rb
+++ lib/bell/public_contact.rb
@@ -9,6 +9,9 @@ module Bell
       validates_format(/^\d{10}$/,
                        :number,
                        :message => Message.contact_number_bad_format(number))
+      if UserContact.find(:number => number)
+        errors.add(:number, Message.contact_number_taken(number))
+      end
     end
   end
 end
--- lib/bell/user_contact.rb
+++ lib/bell/user_contact.rb
@@ -10,6 +10,9 @@ module Bell
       validates_format(/^\d{10}$/,
                        :number,
                        :message => Message.contact_number_bad_format(number))
+      if PublicContact.find(:number => number)
+        errors.add(:number, Message.contact_number_taken(number))
+      end
     end
   end
 end
